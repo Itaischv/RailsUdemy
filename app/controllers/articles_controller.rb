f8864f745dc5a3c1e023feff0a9b8a40c244dc15
Itaischv <itai14v@gmail.com>
2018-10-10 18:21:53 +0300

    Pushing styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article created successfully"
+      flash[:success] = "Article created successfully"
       redirect_to article_path(@article)
 
     else
@@ -30,13 +30,13 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article deleted."
+    flash[:danger] = "Article deleted."
     redirect_to articles_path
   end
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully"
+      flash[:success] = "Article was updated successfully"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -51,7 +51,7 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
-  
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
