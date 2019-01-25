5926cfb38a7a4b11bca20b643c3bb93e7aa486e9
Itaischv <itai14v@gmail.com>
2019-01-26 01:36:50 +0200

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,18 +24,16 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    debugger
     @user = User.new(user_params)
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        flash[:success] = "Welcome to AlphaBlog #{@user.username}"
+        redirect_to articles_path
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
     end
-  end
+
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -69,6 +67,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:username, :email, :password)
     end
 end
