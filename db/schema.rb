494e97f7e4a3394d701153cbd4507b2cefced7e7
Itaischv <itai14v@gmail.com>
2019-01-25 23:13:46 +0200

    adding password authentication for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_210002) do
+ActiveRecord::Schema.define(version: 2019_01_25_211031) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_210002) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
