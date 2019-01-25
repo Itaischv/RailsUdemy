c4b0ffaa5b900a45921c2d17fcfe27a3a47f78e2
Itaischv <itai14v@gmail.com>
2019-01-25 21:59:48 +0200

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_191034) do
+ActiveRecord::Schema.define(version: 2018_10_15_210002) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
